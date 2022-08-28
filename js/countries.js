b193fd4c7ad71328e1382ffe5d6803e6cb625cc8
07arif <ai.arif0711@gmail.com>
2022-08-28 19:33:45 +0600

    International Travel, Display countries name, capital

---
+++ js/countries.js
@@ -0,0 +1,26 @@
+
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountry(data))
+}
+const displayCountry = countries => {
+    // for (const country of countries) {
+    //     console.log(country)
+
+    // }
+    const countriesContainer = document.getElementById('countries-container')
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        console.log(country)
+        countryDiv.innerHTML = `
+<h3>Name: ${country.name.common}</h3>
+<p> Capital: ${country.capital ? country.capital[0] : 'No Capital'} </p>
+`;
+        countriesContainer.appendChild(countryDiv)
+
+    });
+}
+
+loadCountries();
